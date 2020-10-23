5b31cbfd5d145a6c71dc7f6e1848725c228dfc22
BlackH3art <jaras2803@o2.pl>
2020-10-23 19:04:16 +0200

    Footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faMobileAlt, faAt } from '@fortawesome/free-solid-svg-icons';
 
-import facebookLogoFont from '../../img/Facebook-Logo-Font.jpg'
+import facebookLogoFont from '../../img/Facebook-Logo-Font.jpg';
 import instagramLogoFont from '../../img/Instagram-Logo-Font.jpg';
-
+import logo from '../img/logo2-white-other.png';
 
 import './style/footer.css';
 
+
 const Footer = () => {
+
   return ( 
     <>
       <div className="social-buttons-container">
@@ -20,7 +23,39 @@ const Footer = () => {
         </div>
       </div>
       <footer>
+        <div className="style-container">
+           <div className="footer-detail-container">
 
+              <div className="footer-contact-details">
+                <img src={logo} alt="ssssssss"/>
+                <p> <FontAwesomeIcon icon={faMobileAlt} className="footer-icon footer-mobile"/>  507 736 664 </p>
+                <p> <FontAwesomeIcon icon={faAt} className="footer-icon footer-mail"/>  marcin.kubica@gmail.com </p>
+                <div className="social-container">
+                  <a href="https://www.facebook.com/marcin.kubica.735" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebook} className="footer-face footer-icon" /> </a>
+                  <a href="https://www.instagram.com/sniperkmn/?hl=pl" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="footer-insta footer-icon" /> </a>
+                </div>
+              </div>
+
+              <div className="footer-navigation">
+                <nav className="footer-nav">
+                  <h4>Nawigacja</h4>
+                  <ul className="footer-nav-ul">
+                    <li>Strona główna </li>
+                    <li>O mnie </li>
+                    <li className="nav-li-active">Pokemorfozy </li>
+                    <li>Oferta </li>
+                    <li >Blog </li>
+                    <li>Kontakt </li>
+                  </ul>
+                </nav>
+              </div>
+
+              <div className="copyright-container">
+                <p> &copy; 2020 Marcin Kubica Trener Personalny - <a href="none">Polityka prywatności</a> &nbsp; | &nbsp; <a href="none">Regulamin</a> </p>
+              </div>
+
+          </div>
+        </div>
       </footer>
     </>
    );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,48 +1,39 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { Component } from 'react';
+import React from 'react';
 
 import logo from '../img/logo2-white-other.png';
 
-import NavSocial from './NavSocial';
+// import NavSocial from './NavSocial';
 import NavMain from './NavMain';
 
-class Header extends Component {
-  state = {
-    
+const Header = () => {
 
+  const headerStyle = {
+    position: "fixed",
+    backgroundColor:  "rgba(0, 0, 0, 0.5)",
+    height: "80px",
+    width: "100%",
+    boxShadow: "5px 1px 10px rgb(99, 99, 99)",
+    zIndex: '2',
+  }
+  const headerImgStyle = {
+    display: "block",
+    height: "90%",
+    paddingTop: "5px"
   }
 
-  
 
-  render() { 
+  return ( 
+    <>
+      <header style={headerStyle}>
+          <img style={headerImgStyle} src={logo} alt="ssssssss"/>
+          <NavMain />
+          {/* <NavSocial /> */}
+      </header>
+    </>
+    );
 
-    const headerStyle = {
-      position: "fixed",
-      backgroundColor:  "rgba(0, 0, 0, 0.5)",
-      height: "80px",
-      width: "100%",
-      boxShadow: "5px 1px 10px rgb(99, 99, 99)",
-      zIndex: '1',
-    }
-    const headerImgStyle = {
-      display: "block",
-      height: "90%",
-      paddingTop: "5px"
-    }
-
-
-
-    return ( 
-      <>
-        <header style={headerStyle}>
-            <img style={headerImgStyle} src={logo} alt="ssssssss"/>
-            <NavMain />
-            <NavSocial />
-        </header>
-      </>
-     );
-  }
 }
  
 export default Header;
