cc338ab38dd302528c3ae13e8c544531875f4725
BlackH3art <Jaroslaw.Musielak@outlook.com>
2020-11-05 22:18:15 +0100

    bootstrap + final main page styles

--- src/components/body/MainPage.js
+++ src/components/body/MainPage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
-import './styles/mainpage.css'
+import './styles/mainpage.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class MainPage extends Component {
   state = { 
@@ -12,13 +13,16 @@ class MainPage extends Component {
     return ( 
       <>
         <div className="main">
-          <div className="mainContainer">
-            <h1>trenujmy <br/> razem !</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mollis nulla maximus mollis maximus. 
-              Quisque sit amet urna sit amet.
-            </p>
-            <button>zostań podopiecznym <FontAwesomeIcon icon={faAngleDoubleRight} /> </button>
+          <div className="mainContainer">   
+            <h2 className="title-trener"> TRENER PERSONALNY</h2>
+            <h1 className="title-name"> MARCIN KUBICA </h1>
+            <span className="title-text">Zdrowie i zgrabna sylwetka są tuż za rogiem</span>
+   
+          </div>
+          <div className="action-container">
+            <h2 className="action-call">Umów się ze mną!</h2>
+            <span className="action-call-text">Pierwsza konsultacja i trening personalny za darmo!</span>
+            <button className="btn btn-primary p-2 button-action">zostań podopiecznym <FontAwesomeIcon icon={faAngleDoubleRight} /> </button>
           </div>
         </div>
       </>
